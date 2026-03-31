acc93e502abcda6b357558364086a335ee388b72
shih002581 <d11416147@gs.takming.edu.tw>
2026-03-31 16:08:30 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Person
+{
+    public string Name = "姓氏";
+	public Person(string n)
+	{
+        Name = n;
+	}
+}
+public class Student : Person
+{
+    public string stdname = "名字";
+    public Student(string n) : base("陳")
+    {
+        stdname = Name + n;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Class_0331_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Student s = new Student("咚咚");
+            Console.WriteLine(s.Name);
+            Console.WriteLine(s.stdname);
+        }
+    }
+}
